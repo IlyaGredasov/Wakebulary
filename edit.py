2c6359fbf945199352f7035cf456aea1b0af006e
IlyaGredasov <iljagredasov@yandex.ru>
2024-11-19 17:05:16 +0300

    Console ver 1.0
    Stable version

--- edit.py
+++ edit.py
@@ -1,4 +1,25 @@
-import code
 from src.backend.db_client import DataBaseClient
-db = DataBaseClient()
-code.interact(local=locals())
+
+db = DataBaseClient("database.db")
+
+while True:
+    try:
+        lexemes = list(input().split(">"))
+        match lexemes[0]:
+            case "trn":
+                print(db.translate_word(lexemes[1]))
+            case "ins":
+                db.insert_transl(lexemes[1], lexemes[2:])
+                print(f"db.insert_transl({lexemes[1]}, {lexemes[2:]})")
+            case "ers":
+                db.erase_transl(lexemes[1], lexemes[2:])
+                print(f"db.erase_transl({lexemes[1]}, {lexemes[2:]})")
+            case "rep":
+                db.erase_transl(lexemes[1], [lexemes[2]])
+                print(f"db.erase_transl({lexemes[1]}, {[lexemes[2]]})")
+                db.insert_transl(lexemes[1], [lexemes[3]])
+                print(f"db.insert_transl({lexemes[1]}, {[lexemes[3]]})")
+            case "end":
+                break
+    except Exception as e:
+        print(e)
--- src/backend/sample_generator.py
+++ src/backend/sample_generator.py
@@ -31,8 +31,9 @@ class SampleGenerator:
                 sample.append(WordTranslation(word, self.__db.translate_word(word)))
             while sample:
                 question_word = choice(sample)
-                print(f"{question_word.word} ?")
-                print(f"Remain: {len(self.__global_list)}, remain in sample: {len(sample)}, Your answer:")
+                print(f"{question_word.word}?")
+                print(f"Remain: {len(self.__global_list)}, Remain in sample: {len(sample)}, "
+                      f"Correct: {100 * self.session_stats.precision:.1f}%, Your answer:")
                 answer = input().capitalize()
                 if answer == "!end":
                     raise KeyboardInterrupt
